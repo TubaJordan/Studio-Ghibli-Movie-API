83bb9e45d26ea1df4985c3d504ff1b947adb1e43
TubaJordan <113389465+TubaJordan@users.noreply.github.com>
2023-10-07 01:33:16 -0700

    added new endpoint

--- index.js
+++ index.js
@@ -58,6 +58,19 @@ app.get("/documentation", (req, res) => {
     res.sendFile("public/documentation.html", { root: __dirname });
 });
 
+
+app.get("/users/:username", (req, res) => {
+    Users.findOne({ Username: req.params.username })
+        .then((user) => {
+            res.json(user);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error: " + err);
+        });
+});
+
+
 // CREATE - Allow new users to register
 app.post("/users",
     [
